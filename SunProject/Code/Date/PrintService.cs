1d152b53dd8e213e8083b42bcd5166edd00122a5
Migu3e <itaymigul@gmail.com>
2024-05-15 17:21:14 +0300

    Develop, added Main Program & added PrintAllYearDetails to PrintService

--- SunProject/Code/Date/PrintService.cs
+++ SunProject/Code/Date/PrintService.cs
@@ -1,5 +1,5 @@
 namespace SunProject.Code.Date;
-
+using SunProject.Code.Analemma;
 public class PrintService
 {
     public void PrintAllInDate(Date date, double yAxis, double xAxisCorrection, double altitude)
@@ -16,5 +16,22 @@ public class PrintService
         int hour = isSummer ? 12 : 11;
         int minutes = (int)(60 - 19.74 - xAxisCorrection);
         Console.WriteLine($"Time of SunNoon: {hour}:{minutes}");
+    }
+    public void PrintAllYearDetails(int year, double latitude)
+    {
+        Date currentDate = new Date { Day = 1, Month = 1, Year = year };
+        Analemma analemma = new Analemma(latitude);
+
+        while (currentDate.Year == year)
+        {
+            int J = new DateUtilities().CalculateJulianDate(new Date { Day = 1, Month = 1, Year = year }, currentDate);
+            double yAxis = analemma.CalculateYAxis(J);
+            double xAxis = analemma.CalculateXAxis(J);
+            double altitude = analemma.CalculateAltitude(yAxis);
+            double xAxisCorrection = analemma.CorrectXAxis(xAxis);
+        
+            PrintAllInDate(currentDate, yAxis, xAxisCorrection, altitude);
+            currentDate = currentDate.GiveNextDay();
+        }
     }
 }
--- SunProject/Code/Program.cs
+++ SunProject/Code/Program.cs
@@ -1,3 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using SunProject;
+using SunProject.Code.MathUtilities;
+using SunProject.Code.Date;
+using SunProject.Code.Analemma;
+using System;
 
-Console.WriteLine("Hello, World!");
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int year = 2024;
+        double latitude = 32.19306;
+        double longitude = 34.7818;  // Assuming you might use longitude in future expansions
+
+        Analemma analemma = new Analemma(latitude);
+        Date date = new Date
+        {
+            Day = 20,
+            Month = 4,
+            Year = year,
+        };
+
+        // Utilize DateUtilities for Julian date calculation
+        DateUtilities dateUtilities = new DateUtilities();
+
+        // PrintService to handle printing tasks
+        PrintService printService = new PrintService();
+
+        if (date.CheckDate())
+        {
+            int J = dateUtilities.CalculateJulianDate(new Date { Day = 1, Month = 1, Year = year }, date);  // Assuming CalculateJulianDate needs a start and end date
+
+            double yAxis = analemma.CalculateYAxis(J);
+            double xAxis = analemma.CalculateXAxis(J);
+            double altitude = analemma.CalculateAltitude(yAxis);
+            double xAxisCorrection = analemma.CorrectXAxis(xAxis);
+
+            Console.WriteLine("x: " + xAxisCorrection);
+            Console.WriteLine("y: " + yAxis);
+            printService.PrintAllInDate(date, yAxis, xAxisCorrection, altitude);
+        }
+
+        // Print all details for the year 2024 using the PrintService
+        if (date.CheckDate())
+        {
+            printService.PrintAllYearDetails(year, latitude); // Assuming we add latitude handling to PrintAllYearDetails
+        }
+    }
+}

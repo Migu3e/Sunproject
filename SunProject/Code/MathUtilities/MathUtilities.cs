cf4d84bcbcdc59a4f3bcfe3bc54a45605d67066c
Migu3e <itaymigul@gmail.com>
2024-05-15 15:55:29 +0300

    finished the class

--- SunProject/Code/MathUtilities/MathUtilities.cs
+++ SunProject/Code/MathUtilities/MathUtilities.cs
@@ -1,6 +1,10 @@
 namespace SunProject.Code.MathUtilities;
 
-public class MathUtilities
+public static class MathUtilities
 {
-    
+    // Converts an angle from degrees to radians.
+    public static double ConvertDegreesToRadians(double degrees)
+    {
+        return (Math.PI / 180) * degrees;
+    }
 }

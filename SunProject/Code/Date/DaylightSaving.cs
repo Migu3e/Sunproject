ba42b0639e2645059b852e3a782b6bae5afb838e
Migu3e <168545243+Migu3e@users.noreply.github.com>
2024-05-15 18:04:02 +0300

    Revert "Release 1.0"

--- SunProject/Code/Date/DaylightSaving.cs
+++
@@ -1,9 +0,0 @@
-namespace SunProject.Code.Date;
-
-public class DaylightSaving
-{
-    public bool IsSummerTime(Date date)
-    {
-        return date.Month > 3 && date.Month < 10 || (date.Month == 3 && date.Day >= 24) || (date.Month == 10 && date.Day <= 29);
-    }
-}

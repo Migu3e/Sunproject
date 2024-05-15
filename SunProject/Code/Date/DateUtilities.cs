708c8d2cd74f9a34b4d8d4e6bcf10fb28a90e4cc
Migu3e <itaymigul@gmail.com>
2024-05-15 17:01:35 +0300

    DaylightSaving Is Finished

--- SunProject/Code/Date/DateUtilities.cs
+++ SunProject/Code/Date/DateUtilities.cs
@@ -2,5 +2,15 @@ namespace SunProject.Code.Date;
 
 public class DateUtilities
 {
-    
+    public int CalculateJulianDate(Date startDate, Date endDate)
+    {
+        int count = 0;
+        Date tempDate = new Date { Day = startDate.Day, Month = startDate.Month, Year = startDate.Year };
+        while (tempDate.Day != endDate.Day || tempDate.Month != endDate.Month || tempDate.Year != endDate.Year)
+        {
+            tempDate = tempDate.GiveNextDay();
+            count++;
+        }
+        return count;
+    }
 }

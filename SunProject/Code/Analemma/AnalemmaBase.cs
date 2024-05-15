29ad086bb9a8d6db8cbeedbef7df9c030bd3ad92
Migu3e <itaymigul@gmail.com>
2024-05-15 16:33:09 +0300

    AnalemmaBase Is Finished

--- SunProject/Code/Analemma/AnalemmaBase.cs
+++ SunProject/Code/Analemma/AnalemmaBase.cs
@@ -2,5 +2,10 @@ namespace SunProject.Code.Analemma;
 
 public class AnalemmaBase
 {
-    
+    protected double Latitude; // The latitude in degrees.
+
+    protected AnalemmaBase(double latitude)
+    {
+        Latitude = latitude; // Initialize latitude.
+    }
 }
